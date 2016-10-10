14c4c9256b8aa6d9a5c47c38c53d26fc7ad770ed
David Fuentes <david@davoscript.com>
2016-10-10 11:51:16 -0300

    Hidding "yourAPIKey"

--- src/services/weatherservice.js
+++ src/services/weatherservice.js
@@ -2,7 +2,7 @@ var Fetch = require('whatwg-fetch');
 var baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 
 // Add Your API Key
-var apiKey = 'c403f9ef8fb5f14fc34140570cb06716';
+var apiKey = 'yourAPIkey';
 
 var weatherService = {
 	get: function(url) {
